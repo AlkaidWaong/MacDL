0d268b6002fbd947749390603d8bb324fa25495b
ifoz <73516439+AlkaidWaong@users.noreply.github.com>
2024-08-21 09:25:43 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,8 +14,8 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '精品Mac软件',
-  HEO_HERO_TITLE_2: '下载与实用技巧',
-  HEO_HERO_TITLE_3: 'macdl.COM',
+  HEO_HERO_TITLE_2: '下载与教程',
+  HEO_HERO_TITLE_3: 'macdl.com',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
