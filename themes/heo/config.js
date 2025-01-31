301ddda10c3ca8d21789f6acb9ad103b844b6cf7
ifoz <alkaid.waong@outlook.com>
2025-01-31 20:07:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,12 +19,12 @@ const CONFIG = {
   SHOW_SITE_DAYS: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '收录正版、好用的',
-  HEO_HERO_TITLE_2: 'Mac产品',
+  HEO_HERO_TITLE_1: 'Mac效率工具',
+  HEO_HERO_TITLE_2: '分享与下载',
   HEO_HERO_TITLE_3: 'MacAppHQ.COM',
-  HEO_HERO_TITLE_4: '付费应用享折扣',
-  HEO_HERO_TITLE_5: '付费软件专属折扣',
-  HEO_HERO_TITLE_LINK: '/tag/%E4%B8%93%E5%B1%9E%E4%BB%98%E8%B4%B9%E4%BC%98%E6%83%A0',
+  HEO_HERO_TITLE_4: '数码荔枝',
+  HEO_HERO_TITLE_5: '正版软件折扣价',
+  HEO_HERO_TITLE_LINK: 'https://store.lizhi.io/site/index/?cid=j13endai',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -36,7 +36,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://s2.loli.net/2025/01/31/5mIEXbxdOiuyN7r.png', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
